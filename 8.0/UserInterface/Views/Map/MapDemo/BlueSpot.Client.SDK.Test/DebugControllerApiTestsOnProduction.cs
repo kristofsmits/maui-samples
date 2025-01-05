ccde922a9d004157cd9d18188dbebcab2e5e65a9
kristofsmits <kristof.smits@gmail.com>
2025-01-05 11:12:23 +0100

    Fulfilled DebugControllerApiTestsOnProduction
    Fulfilled DebugControllerApiTestsOnProduction

--- 8.0/UserInterface/Views/Map/MapDemo/BlueSpot.Client.SDK.Test/DebugControllerApiTestsOnProduction.cs
+++ 8.0/UserInterface/Views/Map/MapDemo/BlueSpot.Client.SDK.Test/DebugControllerApiTestsOnProduction.cs
@@ -1,29 +1,50 @@
 ﻿using BlueSpot.Client.Sdk.Api;
 using BlueSpot.Client.Sdk.Client;
-using System;
+using BlueSpot.Client.Sdk.Model;
 
 namespace BlueSpot.Client.SDK.Test
 {
     public class DebugControllerApiTestsOnProduction
     {
+
         [Fact]
-        public void PublicHash()
+        public void GetTokenForExistingUserTest()
         {
-            DebugControllerApi api = new DebugControllerApi("https://bluespotserver.nw.r.appspot.com");
-            ApiResponse<string> publicHashResponse = api.HashWithHttpInfo("1234");
-            Assert.Equal("\u0003�gB\u0016��\\v\u001E��U�g�6#ȳ��E�\u0013�x��F�", publicHashResponse.Data);
+            IamClient iamClient = new IamClient("https://bluespotserver.nw.r.appspot.com");
+            string test = iamClient.GetToken("admin","admin");
+            Assert.NotNull(test);
+            Assert.True(test.Length > 0);
         }
 
         [Fact]
-        public void PrivateHash()
+        public void GetTokenForNonExistingUserTest()
         {
+            IamClient iamClient = new IamClient("https://bluespotserver.nw.r.appspot.com");
+            string test = iamClient.GetToken("nonexistinguser", "nonexistinguser_password");
+            Assert.Null(test);
+        }
+
+        [Fact]
+        public void PublicHashTest()
+        {
+            DebugControllerApi api = new DebugControllerApi("https://bluespotserver.nw.r.appspot.com");
+            ApiResponse<DebugResponse> publicHashResponse = api.HashWithHttpInfo("1234");
+            Assert.Equal(true, publicHashResponse.Data.Success);
+            Assert.Equal("\u0003�gB\u0016��\\v\u001E��U�g�6#ȳ��E�\u0013�x��F�", publicHashResponse.Data.Response);
+        }
+
+        [Fact]
+        public void PrivateHashTest()
+        {
+            IamClient iamClient = new IamClient("https://bluespotserver.nw.r.appspot.com");
+            string token = iamClient.GetToken("admin", "admin");
             Configuration configuration = new Configuration();
             configuration.BasePath = "https://bluespotserver.nw.r.appspot.com";
-            configuration.Username = "admin";
-            configuration.Password = "admin";
+            configuration.AccessToken = token;
             DebugControllerApi api = new DebugControllerApi(configuration);
-            ApiResponse<string> publicHashResponse = api.Hash2WithHttpInfo("1234");
-            Assert.Equal("\u0003�gB\u0016��\\v\u001E��U�g�6#ȳ��E�\u0013�x��F�", publicHashResponse.Data);
+            ApiResponse<DebugResponse> publicHashResponse = api.HashWithHttpInfo("1234");
+            Assert.Equal(true, publicHashResponse.Data.Success);
+            Assert.Equal("\u0003�gB\u0016��\\v\u001E��U�g�6#ȳ��E�\u0013�x��F�", publicHashResponse.Data.Response);
         }
     }
 }
---
+++ 8.0/UserInterface/Views/Map/MapDemo/BlueSpot.Client.SDK.Test/IamClient.cs
@@ -0,0 +1,78 @@
+﻿// Copyright 2020 Hysopt NV. All Rights Reserved.
+
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Text;
+
+namespace BlueSpot.Client.SDK.Test
+{
+    public class IamClient
+    {
+        private readonly string iamBasePath;
+
+        public IamClient(string iamBasePath)
+        {
+            this.iamBasePath = iamBasePath;
+        }
+
+        private static dynamic DeserialiseJsonStringToDynamicObject(string json)
+        {
+            dynamic data = JObject.Parse(json);
+            return data;
+        }
+
+        public string GetToken(string username, string password)
+        {
+            try
+            {
+                string clientId = "myapp";
+                string clientSecret = "mysecret";
+                string grantType = "password";
+
+
+                // create request
+                string url = iamBasePath + "/oauth2/token";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                // Add Basic Auth header
+                string authHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"));
+                request.Headers["Authorization"] = $"Basic {authHeader}";
+
+                // Form data
+                string postData = $"grant_type={grantType}&username={username}&password={password}";
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                using (Stream newStream = request.GetRequestStream())
+                {
+                    newStream.Write(byteArray, 0, byteArray.Length);
+
+                    // read the response stream
+                    // inspired by https://stackoverflow.com/questions/137285/what-is-the-best-way-to-read-getresponsestream
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+                        Stream responseStream = response.GetResponseStream();
+                        if (responseStream == null)
+                        {
+                            // Log.Error("Failed to obtain response stream from the IAM response...");
+                            return null;
+                        }
+                        using (StreamReader streamReader = new StreamReader(responseStream, true))
+                        {
+                            string responseBody = streamReader.ReadToEnd();
+                            // responseBody looks like "{\"accessToken\":\"eyJraWQiOiJUSnZOOEpPM2N4XC9ZZW1BaE56WTZ1VlBzRnc4dWY1VlV2NUYzWndpU3pubz0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIzMGMyMmQ5ZS04NGE2LTRkNTktOGU0Ni0zZDcyNGI2OTE0ZTciLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtd2VzdC0xLmFtYXpvbmF3cy5jb21cL2V1LXdlc3QtMV9xWUE4VnlOMUMiLCJjbGllbnRfaWQiOiI2dTA1Zmp1aW1uMXZwb3BvOG9hZWk0NXY4ZCIsIm9yaWdpbl9qdGkiOiI2ZTVjZmJlYS05Yjc0LTQwOTgtYTc4OC05ZjBmNDYyNGE1MDEiLCJldmVudF9pZCI6Ijk3NGFkNzFmLWE2NDctNDQ3MC1iZWIzLTMxMjUwOGRmN2FiYiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2NTY3NTIwMDgsImV4cCI6MTY1Njc1NTYwOCwiaWF0IjoxNjU2NzUyMDA4LCJqdGkiOiI0YjdhODk1NC0wMDI2LTRlMTYtODk1Ni00YmRiZTcxZTU4MTIiLCJ1c2VybmFtZSI6ImtyaXN0b2ZAaHlzb3B0LmNvbSJ9.jk7mzVpYMDV0E1bF-AdcSvhX01wijyNbCVk3Qdokgnprm1u4zCAAlYxLarZmOiuJ-OCmnWOVFEaE11isjnpQKN53K1EzuUxfWn3kluIec99WX5rp8IP2xgisHuZk_IuvvXTqL-1G-k1YFdAOC1oN-8mcjROfqko7Xu916AsLkWELXd6RDFdkyLZ6R8B9aS2KfLmvXprpLe89ye1Tqz-epbG06dO8T-5gKPcHzpeYqOldZMwILgo8iwAYBTgNb-VN6G1z6xyOiM4KW2BBPIujMxAg4RqPf7aDeOToyoPh70bFvvGt3WDtVAqWp0hHUyRTdfxQAKDSvKbgJYOJmaoVag\",\"accessTokenExpirationPeriod\":3600,\"message\":\"Authentication successful\",\"additionalAction\":null,\"mfaSession\":null}"
+                            dynamic data = DeserialiseJsonStringToDynamicObject(responseBody);
+                            return data.access_token;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log.Error("Failed to authenticate username {Username} : {Ex}", username, ex.Message);
+                return null;
+            }
+        }
+    }
+}
